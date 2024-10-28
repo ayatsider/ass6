cafdc1d10c0268de7f0f2cfa3714361c42921337
ayat sider <ayatsdr7@gmail.com>
2024-10-28 19:57:50 +0200

    secend edit

--- Assets/scripts/firstexercise.cs
+++ Assets/scripts/firstexercise.cs
@@ -7,6 +7,7 @@ public class firstexercise : MonoBehaviour
    
     void Start()
     {
+        //exercise number1 Number Generator
         int count=0;
         while(count<20)
         {
@@ -19,6 +20,18 @@ public class firstexercise : MonoBehaviour
             Debug.Log("The number is : "+ num);
             count++;
         }
+
+        // exercise number2 Funny Sentence Generator
+
+        string[] words={"Cat","Dog","Car","Pizze","Hat","Fish","Tree","Monkey","Ball","Bird"};
+        int counter=0;
+        string res=" ";
+        while(counter<7)
+        {
+            res+=words[Random.Range(0,11)]+" ";
+            counter++;
+        }
+        Debug.Log(res);
         
     }
 }
