d756b17c4650bc6a2792ebfe300c93fe2e269400
ayat sider <ayatsdr7@gmail.com>
2024-10-31 16:47:41 +0200

    edit the code

--- Assets/scripts/MultiplicationTable.cs
+++ Assets/scripts/MultiplicationTable.cs
@@ -12,12 +12,18 @@ public class MultiplicationTable : MonoBehaviour
     }
     void Start()
     {
-        for(int i=1;i<=5;i++)
+        // multiplication tables for numbers 1 to 5
+        // for(int i=1;i<=5;i++)
+        // {
+        //     for(int j=1;j<=10;j++)
+        //     {
+        //         Debug.Log(i+"*"+j+"="+i*j);
+        //     }
+        // }
+        
+        for(int i=1;i<=10;i++)
         {
-            for(int j=1;j<=10;j++)
-            {
-                Debug.Log(i+"*"+j+"="+i*j);
-            }
+            Debug.Log(5+"*"+i+"="+i*5);
         }
         
     }
