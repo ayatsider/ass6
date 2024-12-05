27a198b21e2c43a2513b9c90aa727f22a1429265
ayat sider <ayatsdr7@gmail.com>
2024-12-05 16:18:50 +0200

    ass24

---
+++ Assets/scripts/ass24/RaceState.cs
@@ -0,0 +1,14 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+namespace ass24
+{
+    public enum RaceState
+    {
+        Start,
+        Accelerate,
+        Turn,
+        Crash,
+        Finish
+    }
+}
